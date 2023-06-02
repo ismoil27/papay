7f147b3739106134f25132fb1e55491d9890c5d7
ismoil27 <abduraimovismoil98@gmail.com>
2023-06-02 23:22:31 +0900

    feat: router_bssr and controller created

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 const router = require("./router");
 const router_bssr = require("./router_bssr");
 
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
-const store = new MongoDBStore({
-  uri: process.env.MONGO_URL,
-  collection: "sessions",
-});
+// const session = require("express-session");
+// const MongoDBStore = require("connect-mongodb-session")(session);
+// const store = new MongoDBStore({
+//   uri: process.env.MONGO_URL,
+//   collection: "sessions",
+// });
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -16,17 +16,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 2: Session code
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    cookie: {
-      maxAge: 1000 * 60 * 30, // for 30 minutes
-    },
-    store: store,
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     cookie: {
+//       maxAge: 1000 * 60 * 30, // for 30 minutes
+//     },
+//     store: store,
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
 
 // 3: Views code
 app.set("views", "views");
--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -19,6 +19,8 @@ restaurantController.signupProcess = async (req, res) => {
       member = new Member(),
       new_member = await member.signupData(data);
 
+    // SESSION AUTH
+
     res.json({ state: "succeed", data: new_member });
   } catch (err) {
     console.log(`ERROR, cont/signup`);
