5df35e06b460107439ab566fbb2560c390576393
ismoil27 <abduraimovismoil98@gmail.com>
2023-06-06 15:37:37 +0900

    feat: develop session authentication on ejs

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 const router = require("./router");
 const router_bssr = require("./router_bssr");
 
-// const session = require("express-session");
-// const MongoDBStore = require("connect-mongodb-session")(session);
-// const store = new MongoDBStore({
-//   uri: process.env.MONGO_URL,
-//   collection: "sessions",
-// });
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URL,
+  collection: "sessions",
+});
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -16,25 +16,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 2: Session code
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     cookie: {
-//       maxAge: 1000 * 60 * 30, // for 30 minutes
-//     },
-//     store: store,
-//     resave: true,
-//     saveUninitialized: true,
-//   })
-// );
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      maxAge: 1000 * 60 * 30, // for 30 minutes
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(function (req, res, next) {
+  res.locals.member = req.session.member;
+  next();
+});
 
 // 3: Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
-// BSSR -Backend server side rendering
+// BSSR-Backend server side rendering
 
 // 4: Routing code
-app.use("/resto", router_bssr);
+app.use("/resto", router_bssr); //bssr
 app.use("/", router);
 
 module.exports = app;
--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -2,6 +2,17 @@ const Member = require("../models/Member");
 
 let restaurantController = module.exports;
 
+restaurantController.getMyRestaurantData = async (req, res) => {
+  try {
+    console.log("GET: cont/getMyRestaurantData");
+    // TODO: Get my restaurant product
+    res.render("restaurant-menu");
+  } catch (err) {
+    console.log(`ERROR, cont/getMyRestaurantData`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
+
 restaurantController.getSignupMyRestaurant = async (req, res) => {
   try {
     console.log("GET: cont/getSignupMyRestaurant");
@@ -19,9 +30,9 @@ restaurantController.signupProcess = async (req, res) => {
       member = new Member(),
       new_member = await member.signupData(data);
 
-    // SESSION AUTH
+    req.session.member = new_member;
 
-    res.json({ state: "succeed", data: new_member });
+    res.redirect("/resto/products/menu");
   } catch (err) {
     console.log(`ERROR, cont/signup`);
     res.json({ state: "fail", message: err.message });
@@ -45,7 +56,10 @@ restaurantController.loginProcess = async (req, res) => {
       member = new Member(),
       result = await member.loginData(data);
 
-    res.json({ state: "succeed", data: result });
+    req.session.member = result;
+    req.session.save(function () {
+      res.redirect("/resto/products/menu");
+    });
   } catch (err) {
     console.log(`ERROR, cont/login`);
     res.json({ state: "fail", message: err.message });
@@ -55,4 +69,12 @@ restaurantController.loginProcess = async (req, res) => {
 restaurantController.logoutProcess = (req, res) => {
   console.log("GET: cont/logout");
   res.send("logout sahifasidasiz");
+};
+
+restaurantController.checkSessions = (req, res) => {
+  if (req.session?.member) {
+    res.json({ state: "succeed", data: req.session.member });
+  } else {
+    res.json({ state: "fail", message: "You are not authenticated" });
+  }
 };
