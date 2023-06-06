52e47621193f4a855022dc7cc9db7e29706b2b06
ismoil27 <abduraimovismoil98@gmail.com>
2023-06-06 16:48:49 +0900

    feat: develop product controller logic

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -71,6 +71,18 @@ restaurantController.logoutProcess = (req, res) => {
   res.send("logout sahifasidasiz");
 };
 
+restaurantController.validateAuthRestaurant = (req, res, next) => {
+  if (req.session?.member?.mb_type === "RESTAURANT") {
+    req.member = req.session.member;
+    next();
+  } else {
+    res.json({
+      state: "fail",
+      message: "only authenticated members with restaurant",
+    });
+  }
+};
+
 restaurantController.checkSessions = (req, res) => {
   if (req.session?.member) {
     res.json({ state: "succeed", data: req.session.member });
