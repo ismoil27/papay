0ec9f5e392457aaaa96ba1447f9c7cba84110f6a
ismoil27 <abduraimovismoil98@gmail.com>
2023-06-16 17:36:22 +0900

    fix: develop business logic of signup  page on adminka

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,5 +1,7 @@
+const assert = require("assert");
 const Member = require("../models/Member");
 const Product = require("../models/Product");
+const Definer = require("../lib/mistake");
 
 let restaurantController = module.exports;
 
@@ -43,12 +45,17 @@ restaurantController.getSignupMyRestaurant = async (req, res) => {
 restaurantController.signupProcess = async (req, res) => {
   try {
     console.log("POST: cont/signupProcess");
-    const data = req.body,
-      member = new Member(),
-      new_member = await member.signupData(data);
+    assert(req.file, Definer.general_err3);
 
-    req.session.member = new_member;
+    let new_member = req.body;
+    new_member.mb_type = "RESTAURANT";
+    new_member.mb_image = req.file.path;
 
+    const member = new Member();
+    const result = await member.signupData(new_member);
+    assert(result, Definer.general_err1);
+
+    req.session.member = result;
     res.redirect("/resto/products/menu");
   } catch (err) {
     console.log(`ERROR, cont/signup, ${err.message}`);
