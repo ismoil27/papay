870e1b7fd7ccae8444b3c1cacd7a09f2bf6eb894
ismoil27 <abduraimovismoil98@gmail.com>
2023-06-24 01:05:50 +0900

    feat: develop resrtaurant member top data by admin

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -145,11 +145,25 @@ restaurantController.getAllRestaurants = async (req, res) => {
 
     const restaurant = new Restaurant();
     const restaurants_data = await restaurant.getAllRestaurantsData();
-    console.log("restaurants_data", restaurants_data);
+    // console.log("restaurants_data", restaurants_data);
 
     res.render("all-restaurants", { restaurants_data: restaurants_data });
   } catch (err) {
     console.log(`ERROR, cont/getAllRestaurants, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
+
+restaurantController.updateRestaurantByAdmin = async (req, res) => {
+  try {
+    console.log("GET: cont/updateRestaurantByAdmin");
+
+    const restaurant = new Restaurant();
+    const result = await restaurant.updateRestaurantByAdminData(req.body);
+
+    await res.json({ state: "success", data: result });
+  } catch (err) {
+    console.log(`ERROR, cont/updateRestaurantByAdmin, ${err.message}`);
     res.json({ state: "fail", message: err.message });
   }
 };
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,6 +1,7 @@
 const assert = require("assert");
 const MemberModel = require("../schema/member.model");
 const Definer = require("../lib/mistake");
+const { shapeIntoMongooseObjectId } = require("../lib/config");
 
 class Restaurant {
   constructor() {
@@ -15,6 +16,25 @@ class Restaurant {
         })
         .exec();
       assert(result, Definer.general_err1);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async updateRestaurantByAdminData(update_date) {
+    try {
+      const id = shapeIntoMongooseObjectId(update_date?.id);
+      const result = await this.memberModel
+        .findByIdAndUpdate({ _id: id }, update_date, {
+          runValidators: true,
+          lean: true,
+          returnDocument: "after",
+        })
+        .exec();
+
+      assert.ok(result, Definer.general_err1);
+
       return result;
     } catch (err) {
       throw err;
