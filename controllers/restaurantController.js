5db0db5321f232e268041eded1e6da03ef0b804b
ismoil27 <abduraimovismoil98@gmail.com>
2023-06-23 19:02:05 +0900

    fix: modify to render real restaurant data

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -2,6 +2,7 @@ const assert = require("assert");
 const Member = require("../models/Member");
 const Product = require("../models/Product");
 const Definer = require("../lib/mistake");
+const Restaurant = require("../models/Restaurant");
 
 let restaurantController = module.exports;
 
@@ -138,10 +139,15 @@ restaurantController.validateAdmin = (req, res, next) => {
   }
 };
 
-restaurantController.getAllRestaurants = (req, res) => {
+restaurantController.getAllRestaurants = async (req, res) => {
   try {
     console.log("GET: cont/getAllRestaurants");
-    res.render("all-restaurants");
+
+    const restaurant = new Restaurant();
+    const restaurants_data = await restaurant.getAllRestaurantsData();
+    console.log("restaurants_data", restaurants_data);
+
+    res.render("all-restaurants", { restaurants_data: restaurants_data });
   } catch (err) {
     console.log(`ERROR, cont/getAllRestaurants, ${err.message}`);
     res.json({ state: "fail", message: err.message });
